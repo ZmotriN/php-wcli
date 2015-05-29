018b05a098d269e2cef5980cb6827e5ad1a6ae12
ZmotriN <zmotrin@gmail.com>
2015-05-28 23:56:47 -0400

    remove useless comments

--- wcli.c
+++ wcli.c
@@ -1,3 +1,21 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2015 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Maxime Larriv√©e-Roy <zmotrin@gmail.com>                                |
+  +----------------------------------------------------------------------+
+*/
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -12,24 +30,6 @@
 
 ZEND_DECLARE_MODULE_GLOBALS(wcli)
 
-
-/* {{{ PHP_INI
- */
-/* Remove comments and fill if you need to have entries in php.ini
-PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("wcli.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_wcli_globals, wcli_globals)
-    STD_PHP_INI_ENTRY("wcli.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_wcli_globals, wcli_globals)
-PHP_INI_END()
-*/
-/* }}} */
-
-/* Remove the following function when you have successfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_wcli_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
 PHP_FUNCTION(confirm_wcli_compiled)
 {
 	char *arg = NULL;
@@ -43,25 +43,6 @@ PHP_FUNCTION(confirm_wcli_compiled)
 	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "wcli", arg);
 	RETURN_STRINGL(strg, len, 0);
 }
-/* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and 
-   unfold functions in source code. See the corresponding marks just before 
-   function definition, where the functions purpose is also documented. Please 
-   follow this convention for the convenience of others editing your code.
-*/
-
-/* __function_stubs_here__ */
-
-/* {{{ php_wcli_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_wcli_init_globals(zend_wcli_globals *wcli_globals)
-{
-	wcli_globals->global_value = 0;
-	wcli_globals->global_string = NULL;
-}
-*/
-/* }}} */
 
 
 static void php_wcli_init_globals(zend_wcli_globals *wcli_globals) {}
@@ -82,27 +63,19 @@ PHP_MINIT_FUNCTION(wcli)
 	REGISTER_LONG_CONSTANT("Black", 0x00, CONST_CS|CONST_PERSISTENT);
 	REGISTER_LONG_CONSTANT("Grey", FOREGROUND_INTENSITY, CONST_CS|CONST_PERSISTENT);
 
-    ZEND_INIT_MODULE_GLOBALS(wcli, php_wcli_init_globals, NULL);
-    //REGISTER_INI_ENTRIES();
-
-    return SUCCESS;
+  ZEND_INIT_MODULE_GLOBALS(wcli, php_wcli_init_globals, NULL);
+  //REGISTER_INI_ENTRIES();
+  return SUCCESS;
 }
-/* }}} */
 
-/* {{{ PHP_MSHUTDOWN_FUNCTION
- */
+
 PHP_MSHUTDOWN_FUNCTION(wcli)
 {
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
 	return SUCCESS;
 }
-/* }}} */
 
-/* Remove if there's nothing to do at request start */
-/* {{{ PHP_RINIT_FUNCTION
- */
+
+
 PHP_RINIT_FUNCTION(wcli)
 {
 	WCLI_G(chnd) = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -123,11 +96,9 @@ PHP_RINIT_FUNCTION(wcli)
 	}
 	return SUCCESS;
 }
-/* }}} */
 
-/* Remove if there's nothing to do at request end */
-/* {{{ PHP_RSHUTDOWN_FUNCTION
- */
+
+
 PHP_RSHUTDOWN_FUNCTION(wcli)
 {
 	if(WCLI_G(console)){
@@ -138,26 +109,18 @@ PHP_RSHUTDOWN_FUNCTION(wcli)
 	
 	return SUCCESS;
 }
-/* }}} */
 
-/* {{{ PHP_MINFO_FUNCTION
- */
+
+
 PHP_MINFO_FUNCTION(wcli)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "WCLI support", "enabled");
 	php_info_print_table_end();
-
-	/* Remove comments if you have entries in php.ini
-	DISPLAY_INI_ENTRIES();
-	*/
 }
-/* }}} */
 
-/* {{{ wcli_functions[]
- *
- * Every user visible function must have an entry in wcli_functions[].
- */
+
+
 const zend_function_entry wcli_functions[] = {
 	PHP_FE(wcli_get_output_handle,	NULL)
 	PHP_FE(wcli_get_input_handle,	NULL)
@@ -220,13 +183,11 @@ const zend_function_entry wcli_functions[] = {
 
 	PHP_FE(wcli_test, NULL)
 
-	/* __function_entries_here__ */
 	{NULL, NULL, NULL}
 };
-/* }}} */
 
-/* {{{ wcli_module_entry
- */
+
+
 zend_module_entry wcli_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"wcli",
@@ -239,11 +200,13 @@ zend_module_entry wcli_module_entry = {
 	PHP_WCLI_VERSION,
 	STANDARD_MODULE_PROPERTIES
 };
-/* }}} */
+
 
 #ifdef COMPILE_DL_WCLI
 ZEND_GET_MODULE(wcli)
 #endif
+
+
 
 
 // ********************************************************************
@@ -677,7 +640,7 @@ PHP_FUNCTION(wcli_clear) {
 	pos.Y = 0;
 
 
-	// ‡ revoir, pas sur pour les set cursor position
+	// √† revoir, pas sur pour les set cursor position
 
 
 	if(!WCLI_G(console)) RETURN_BOOL(0);
@@ -686,7 +649,7 @@ PHP_FUNCTION(wcli_clear) {
 	if(!FillConsoleOutputAttribute(WCLI_G(chnd),info.wAttributes,info.dwSize.X*info.dwSize.Y,pos,&nwc))  RETURN_BOOL(0);
 	if(!FillConsoleOutputCharacter(WCLI_G(chnd),32,info.dwSize.X*info.dwSize.Y,pos,&nwc)) RETURN_BOOL(0);
 	if(!SetConsoleCursorPosition(WCLI_G(chnd),pos)) RETURN_BOOL(0);
-	if(!SetConsoleTextAttribute(WCLI_G(chnd), info.wAttributes)) RETURN_BOOL(0); // pas sur de celle l‡
+	if(!SetConsoleTextAttribute(WCLI_G(chnd), info.wAttributes)) RETURN_BOOL(0); // pas sur de celle l√†
 	RETURN_BOOL(1);
 }
 
