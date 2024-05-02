f9ab7eb676729a0571f292d09f6e90d168c95b34
Maxime Larrivée-Roy <zmotrin@gmail.com>
2024-05-02 16:51:07 -0400

    Use VK_ESCAPE in progressbar example

--- examples/progressbar.php
+++ examples/progressbar.php
@@ -14,5 +14,5 @@ for($i = 0; $i <= 100; $i += 0.1, usleep(100000)) {
     $j = $i * 10;
     if($j <= 100) $pb1->draw($j / 100, round($j) == 100 ? 1 : 0);
     $pb2->draw($i / 100, round($i) == 100 ? 1 : 0);
-    if(wcli_get_key_async() == 0x1B) break;
+    if(wcli_get_key_async() == VK_ESCAPE) break;
 }
